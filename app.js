354f864901ad4788da031c54cf938f0f3761d344
Kolby Rogers <kolbyrogers1@gmail.com>
2022-02-11 08:33:17 -0700

    foo

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 var app = new Vue({
-  el: "#app",
-  data: {},
-  methods: {},
+	el: "#app",
+	vuetify: new Vuetify(),
+	data: () => ({
+		valid: false,
+		firstname: "",
+		lastname: "",
+		nameRules: [(v) => !!v || "Name is required"],
+		date: null,
+		menu: false,
+	}),
+	methods: {
+		save(date) {
+			this.$refs.menu.save(date);
+		},
+	},
 });
